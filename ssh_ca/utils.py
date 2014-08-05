1225aab60ad7062613b5081f0beeca74f24cc1f0
Aaron Turner <synfinatic@gmail.com>
2014-08-05 12:23:34 -0700

    Update code with feedback from Mark & Mike Add unit tests for utils.py

--- ssh_ca/utils.py
+++ ssh_ca/utils.py
@@ -1,10 +1,11 @@
 import sys
 import time
+import unittest
 
 def convert_relative_time(time_string):
     """Takes a single +XXXX[smhdw] string and converts to seconds"""
-    last_char = time_string[len(time_string) - 1]
-    user_value = time_string[0:(len(time_string) - 1)]
+    last_char = time_string[-1]
+    user_value = time_string[0:-1]
     if last_char == 's':
         seconds = int(user_value)
     elif last_char == 'm':
@@ -22,16 +23,14 @@ def convert_relative_time(time_string):
     return seconds
 
 def parse_time(time_string, reference_time=int(time.time())):
-    """Parses a time in YYYYMMDDHHMMSS or +XXXX[smhdw][...] and returns
-    epoch time.  Just like ssk-keygen, we allow complex expressions like
-    +5d7h37m for 5 days, 7 hours, 37 minutes.
+    """Parses a time in YYYYMMDDHHMMSS or +XXXX[smhdw][...]
+
+    Returns epoch time.  Just like ssk-keygen, we allow complex 
+    expressions like +5d7h37m for 5 days, 7 hours, 37 minutes.
 
     reference_time should be the epoch time used for calculating
     the time when using +XXXX[smhdw][...], defaults to now.
-
-    if time_string == 0, returns None"""
-    if time_string == '0':
-        return None
+    """
 
     seconds = 0
     if time_string[0] == '+' or time_string[0] == '-':
@@ -60,7 +59,7 @@ def parse_time(time_string, reference_time=int(time.time())):
     else:
         # parse YYYYMMDDHHMMSS
         struct_time = time.strptime(time_string, "%Y%m%d%H%M%S")
-        epoch = int(time.strftime("%s", struct_time))
+        epoch = int(time.mktime(struct_time))
 
     return epoch
 
@@ -71,6 +70,39 @@ def epoch2timefmt(epoch):
     understand DST so it will add an hour for you. :-/
     """
     struct_time = time.localtime(epoch)
-    if struct_time.st_isdst == 1:
+    if struct_time.tm_isdst == 1:
         struct_time = time.localtime(epoch - 3600)
     return time.strftime("%Y%m%d%H%M%S", struct_time)
+
+
+class ParseTimeTests(unittest.TestCase):
+    def setUp(self):
+        self.now = int(time.time())
+
+    def test_one_week(self):
+        one_week = parse_time("+1w", self.now)
+        one_week_check = self.now + (60 * 60 * 24 * 7)
+        self.assertEqual(one_week_check, one_week)
+
+    def test_one_day(self):
+        one_day = parse_time("+1d3h15s", self.now)
+        one_day_check = self.now + (60 * 60 * 27) + 15
+        self.assertEqual(one_day_check, one_day)
+
+    def test_two_thirty(self):
+        two_thirty = parse_time("+2h30m", self.now)
+        two_thirty_check = self.now + (60 * 60 * 2.5)
+        self.assertEqual(two_thirty_check, two_thirty)
+
+    def test_epoch2timefmt(self):
+        struct_time = time.localtime(self.now)
+        offset = 0
+        if struct_time.tm_isdst == 1:
+            offset = 3600
+
+        longtime = epoch2timefmt(self.now + offset)
+        rightnow = parse_time(longtime)
+        self.assertEqual(rightnow, self.now)
+
+if __name__ == '__main__':
+    unittest.main()
